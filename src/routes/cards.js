de9db91602c5fd07b48b23932e5392a4c5d3d74b
Daniel Hernandez <dhdan97@gmail.com>
2022-05-19 12:49:06 -0400

    Added getUserCards endpoint

--- src/routes/cards.js
+++ src/routes/cards.js
@@ -9,9 +9,11 @@ const verifyEmployee = require('./middlewares/verifyEmployee');
 const verifyCreator = require('./middlewares/verifyCreator');
 const getCard = require('./controllers/cards/getCard');
 const getAllCards = require('./controllers/cards/getAllCards');
+const getUserCards = require('./controllers/cards/getUserCards');
 
 router.post('/', authenticateToken, createCard);
 router.get('/getAllCards', getAllCards);
+router.get(`/getUserCards`, authenticateToken, getUserCards);
 router.get('/:id', getCard);
 router.put('/:id', authenticateToken, verifyCreator, updateCard);
 router.delete('/:id', authenticateToken, verifyEmployee, deactivateCard);
