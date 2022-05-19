caedad0c664acec34959952069084997f81b7b0b
abdulimtiaz <abdulimtiaz98@gmail.com>
2022-05-19 00:02:03 -0400

    added get all cards endpoint

---
+++ src/routes/controllers/cards/getAllCards.js
@@ -0,0 +1,21 @@
+const { pool } = require('../../../utils/db');
+
+const getAllCards = async (req, res) => {
+    try {
+        let cards;
+
+        if (req.query.active === true) {
+            cards = await pool.query(`SELECT * FROM Card_ WHERE activeStatus = TRUE`);
+        } else {
+            cards = await pool.query(`SELECT * FROM Card_`);
+        }
+
+        return res.status(200).json(cards.rows);
+
+    } catch (err) {
+        console.log(`Error in ${__filename}:\n`, err);
+        return res.status(500).json({ msg: 'Internal server error' });
+    }
+};
+
+module.exports = getAllCards;
